b7afe2ce9b36af9458a215347ca8631dd1dad3e9
kylejohnsonks <92126368+kylejohnsonks@users.noreply.github.com>
2022-01-27 08:10:16 -0600

    Update logic.js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 // console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -16,15 +16,35 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+  // Add GeoJSON data.
+  let sanFranAirport =
+  {"type":"FeatureCollection","features":[{
+      "type":"Feature",
+      "properties":{
+          "id":"3469",
+          "name":"San Francisco International Airport",
+          "city":"San Francisco",
+          "country":"United States",
+          "faa":"SFO",
+          "icao":"KSFO",
+          "alt":"13",
+          "tz-offset":"-8",
+          "dst":"A",
+          "tz":"America/Los_Angeles"},
+          "geometry":{
+              "type":"Point",
+              "coordinates":[-122.375,37.61899948120117]}}
+  ]};
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "red"
-  }).addTo(map);
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport).addTo(map);
+
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport,{
+  pointToLayer: function(feature, latlng) {
+    return L.marker(latlng)
+    .bindPopup("<h2>" + feature.properties.city + "</h2>");
+  }
+}).addTo(map);
+
