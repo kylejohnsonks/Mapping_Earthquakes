9e993cc3c733f26f4c41120c31e3e4365384e602
kylejohnsonks <92126368+kylejohnsonks@users.noreply.github.com>
2022-01-26 18:03:08 -0600

    Update logic.js

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -16,13 +16,40 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// An array containing each city's location, state, and population.
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
 
-// Add a circle
-L.circle([34.0522, -118.2437], {
-    radius: 1000
- }).addTo(map);
-
-
- L.circleMarker([34.0522, -118.2437]).addTo(map);
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+   });
